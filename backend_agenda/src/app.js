931c5c35beab38f227650ec20a9310fdeb608356
Emilio <emi-421@hotmail.com>
2026-04-18 01:48:24 -0600

    Intento9Backend

--- backend_agenda/src/app.js
+++ backend_agenda/src/app.js
@@ -11,7 +11,7 @@ app.use('/api', contactosRoutes);
 // 🚨 PUERTO DINÁMICO (CLAVE)
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
