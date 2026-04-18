0dcb306e651d3b77aedd0c693c4fba5670781f08
Emilio Ortiz Romo <141103869+3m1l1o37@users.noreply.github.com>
2026-04-18 17:55:17 -0600

    Rehaciendo conexion a base de datos

--- backend_agenda/src/db/connection.js
+++ backend_agenda/src/db/connection.js
@@ -3,17 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const dbUrl = new URL(process.env.MYSQL_URL);
-// console.log("MYSQL_URL:", process.env.MYSQL_URL);
-// const pool = mysql.createPool({
-//   host: dbUrl.hostname,
-//   user: dbUrl.username,
-//   password: dbUrl.password,
-//   database: dbUrl.pathname.replace('/', ''),
-//   port: dbUrl.port,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-// });
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT) || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 
-console.log("SIN DB TEST");
-//export default pool;
+export default pool;
