975a68d2bb8d1d1d6302353649842307335325ac
Emilio <emi-421@hotmail.com>
2026-04-17 23:28:48 -0600

    cambio-en-el-pool2

--- backend_agenda/src/db/connection.js
+++ backend_agenda/src/db/connection.js
@@ -3,6 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const dbUrl = new URL(process.env.DATABASE_URL);
 
+const pool = mysql.createPool({
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.replace('/', ''),
+  port: dbUrl.port,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 export default pool;
