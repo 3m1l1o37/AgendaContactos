2b2ec270ea61ac871376b8afe623afc89ae6ced2
Emilio Ortiz Romo <141103869+3m1l1o37@users.noreply.github.com>
2026-04-18 17:56:10 -0600

    Eliminacion de comentario para poder conectar a base de datos

--- backend_agenda/src/controllers/contactos.controller.js
+++ backend_agenda/src/controllers/contactos.controller.js
@@ -1,4 +1,4 @@
-//import  pool  from '../db/connection.js';
+import  pool  from '../db/connection.js';
 
 export const getContactos = async (req, res) => {
   const [rows] = await pool.query('SELECT * FROM contactos');
