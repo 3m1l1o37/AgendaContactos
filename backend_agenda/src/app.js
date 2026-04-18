48d03d73b1adca96b856d130484db9e120e0bc83
Emilio <emi-421@hotmail.com>
2026-04-18 01:46:07 -0600

    Intento9Backend

--- backend_agenda/src/app.js
+++ backend_agenda/src/app.js
@@ -1,18 +1,17 @@
-// import express from 'express';
-// import cors from 'cors';
-// import contactosRoutes from './routes/contactos.routes.js';
-// const app = express();
+import express from 'express';
+import cors from 'cors';
+import contactosRoutes from './routes/contactos.routes.js';
+const app = express();
 
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-// app.use('/api', contactosRoutes);
+app.use('/api', contactosRoutes);
 
-// // 🚨 PUERTO DINÁMICO (CLAVE)
-// const PORT = process.env.PORT || 3000;
+// 🚨 PUERTO DINÁMICO (CLAVE)
+const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//   console.log(`Servidor corriendo en puerto ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
-console.log("🔥 NUEVO DEPLOY ACTIVADO 🔥");
