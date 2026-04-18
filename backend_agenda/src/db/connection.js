abaca4ea254977433b055d4ad9c8a7298b0f3bf5
Emilio <emi-421@hotmail.com>
2026-04-18 02:10:43 -0600

    Intento9Backend

--- backend_agenda/src/db/connection.js
+++ backend_agenda/src/db/connection.js
@@ -3,15 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const dbUrl = new URL(process.env.MYSQL_URL);
-console.log("MYSQL_URL:", process.env.MYSQL_URL);
-const pool = mysql.createPool({
-  host: dbUrl.hostname,
-  user: dbUrl.username,
-  password: dbUrl.password,
-  database: dbUrl.pathname.replace('/', ''),
-  port: dbUrl.port,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+// const dbUrl = new URL(process.env.MYSQL_URL);
+// console.log("MYSQL_URL:", process.env.MYSQL_URL);
+// const pool = mysql.createPool({
+//   host: dbUrl.hostname,
+//   user: dbUrl.username,
+//   password: dbUrl.password,
+//   database: dbUrl.pathname.replace('/', ''),
+//   port: dbUrl.port,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// });
+
+console.log("SIN DB TEST");
 export default pool;
