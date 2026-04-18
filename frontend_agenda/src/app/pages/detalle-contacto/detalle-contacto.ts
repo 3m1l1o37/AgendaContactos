af3ce3864085b5781224f752b4f7f601a5fefdf2
Emilio <emi-421@hotmail.com>
2026-04-17 21:52:59 -0600

    Footer-Frontend-Styles-Contacto-Styles

--- frontend_agenda/src/app/pages/detalle-contacto/detalle-contacto.ts
+++ frontend_agenda/src/app/pages/detalle-contacto/detalle-contacto.ts
@@ -2,16 +2,21 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ContactosService } from '../../services/contactos';
 import { ChangeDetectorRef } from '@angular/core';
+import { AvatarService } from "../../services/avatar.service.ts";
+
 
 @Component({
   standalone: true,
   templateUrl: './detalle-contacto.html',
+  styleUrls: ['./detalle-contacto.css']
+
 })
 export class DetalleContacto implements OnInit {
 
   route = inject(ActivatedRoute);
   servicio = inject(ContactosService);
   cdr = inject(ChangeDetectorRef);
+  avatarService = inject(AvatarService);
 
   contacto: any;
   cargando = true;
