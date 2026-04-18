fe9b8a726103e536474209248c328ceb97fb2ce9
Emilio <emi-421@hotmail.com>
2026-04-17 23:01:18 -0600

    Primer-intento-de-backend

--- backend_agenda/src/app.js
+++ backend_agenda/src/app.js
@@ -9,25 +9,9 @@ app.use(express.json());
 
 app.use('/api', contactosRoutes);
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en puerto 3000');
-});
-//.get para el detalle de los contactos
-app.get('/api/contactos/:id', async (req, res) => {
-  const { id } = req.params;
+// 🚨 PUERTO DINÁMICO (CLAVE)
+const PORT = process.env.PORT || 3000;
 
-  try {
-    const [rows] = await pool.query(
-      'SELECT * FROM contactos WHERE id = ?',
-      [id]
-    );
-
-    if (rows.length === 0) {
-      return res.status(404).json({ message: 'Contacto no encontrado' });
-    }
-
-    res.json(rows[0]);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
