81e8113c9a04cae0472554a49e6e14a21b5cdb7a
Emilio Ortiz Romo <141103869+3m1l1o37@users.noreply.github.com>
2026-04-18 18:38:30 -0600

    Codigo limpio CORS

--- backend_agenda/src/app.js
+++ backend_agenda/src/app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import contactosRoutes from './routes/contactos.routes.js';
+
 const app = express();
 
 app.use(cors({
   origin: [
     'http://localhost:4200',
-    'https://agendacontactose.netlify.app/'  // ← cambia por tu URL de Netlify
+    'https://agendacontactose.netlify.app'
   ]
 }));
+
 app.use(express.json());
 
 app.use('/api', contactosRoutes);
@@ -16,11 +18,11 @@ app.use('/api', contactosRoutes);
 app.get('/ping', (req, res) => {
   res.json({ ok: true });
 });
-// 🚨 PUERTO DINÁMICO (CLAVE)
+
 const PORT = process.env.PORT || 3000;
 console.log("PORT REAL:", process.env.PORT);
-app.listen(PORT,  () => {
+
+app.listen(PORT, () => {
   console.log("PORT REAL:", process.env.PORT);
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-
