d6a6240dddc8f356b0b062f52abfc795746f9be2
Emilio Ortiz Romo <141103869+3m1l1o37@users.noreply.github.com>
2026-04-18 18:23:13 -0600

    Reorganizacion de los metodos de conexion

--- backend_agenda/src/controllers/contactos.controller.js
+++ backend_agenda/src/controllers/contactos.controller.js
@@ -1,47 +1,40 @@
-import  pool  from '../db/connection.js';
+import pool from '../db/connection.js';
 
 export const getContactos = async (req, res) => {
   const [rows] = await pool.query('SELECT * FROM contactos');
   res.json(rows);
 };
 
-/*export const getContacto = async (req, res) => {
+export const getContacto = async (req, res) => {
   const { id } = req.params;
   const [rows] = await pool.query(
-  'SELECT * FROM contactos WHERE id = ?',
-  [id]
+    'SELECT * FROM contactos WHERE id = ?',
+    [id]
   );
   res.json(rows[0]);
-  res.json([{ ok: true }]);
-};*/
+};
 
 export const createContacto = async (req, res) => {
   const { nombre, telefono, email, pais } = req.body;
-
   const [result] = await pool.query(
     'INSERT INTO contactos(nombre, telefono, email, pais) VALUES (?, ?, ?, ?)',
     [nombre, telefono, email, pais]
   );
-
   res.json({ id: result.insertId, nombre, telefono, email, pais });
 };
 
 export const updateContacto = async (req, res) => {
   const { id } = req.params;
   const { nombre, telefono, email, pais } = req.body;
-
   await pool.query(
     'UPDATE contactos SET nombre=?, telefono=?, email=?, pais=? WHERE id=?',
     [nombre, telefono, email, pais, id]
   );
-
   res.sendStatus(204);
 };
 
 export const deleteContacto = async (req, res) => {
   const { id } = req.params;
-
   await pool.query('DELETE FROM contactos WHERE id=?', [id]);
-
   res.sendStatus(204);
 };
