c17245b1d6860a7b3faacbc0191812fb3ecb2c51
Emilio Ortiz Romo <141103869+3m1l1o37@users.noreply.github.com>
2026-04-18 18:36:25 -0600

    Contactos usa Backend API
    Refactored ContactosService to fetch contacts from a backend API and return observables for adding and updating contacts.

--- frontend_agenda/src/app/services/contactos.ts
+++ frontend_agenda/src/app/services/contactos.ts
@@ -1,30 +1,3 @@
-// import { Injectable, signal } from '@angular/core';
-// import { Contacto } from '../models/contacto';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ContactosService {
-
-//   //esto son datos locales
-//    contactos = signal<Contacto[]>([
-//      { id: 1, nombre: 'Juan', telefono: '1234567890', email: 'juan@gmail.com' },
-//      { id: 2, nombre: 'Maria', telefono: '9876543210', email: 'maria@gmail.com' }
-//    ]);
-
-//   getContactos() {
-//     return this.contactos;
-//   }
-
-//   eliminar(id: number) {
-//     this.contactos.update(lista => lista.filter(c => c.id !== id));
-//   }
-//   actualizar(contactoActualizado: any) {
-//   this.contactos.update(lista =>
-//     lista.map(c => c.id === contactoActualizado.id ? contactoActualizado : c));
-//   }
-// }
-
 import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Contacto } from '../models/contacto';
@@ -35,12 +8,11 @@ import { Contacto } from '../models/contacto';
 export class ContactosService {
 
   private apiUrl = 'https://endearing-reprieve-production-b1c7.up.railway.app/api/contactos';
-  
+
   contactos = signal<Contacto[]>([]);
 
   constructor(private http: HttpClient) {}
 
-  //  Obtener contactos desde backend
   cargarContactos() {
     this.http.get<Contacto[]>(this.apiUrl)
       .subscribe(data => {
@@ -48,29 +20,20 @@ export class ContactosService {
       });
   }
 
-  //  Agregar contacto
   agregar(contacto: Omit<Contacto, 'id'>) {
-    this.http.post(this.apiUrl, contacto)
-      .subscribe(() => this.cargarContactos());
+    return this.http.post(this.apiUrl, contacto);
   }
 
-  //  Actualizar contacto
   actualizar(contacto: Contacto) {
-    this.http.put(`${this.apiUrl}/${contacto.id}`, contacto)
-      .subscribe(() => this.cargarContactos());
+    return this.http.put(`${this.apiUrl}/${contacto.id}`, contacto);
   }
 
-  //  Eliminar contacto
   eliminar(id: number) {
     this.http.delete(`${this.apiUrl}/${id}`)
       .subscribe(() => this.cargarContactos());
   }
 
-  //obtener por id los detalles de un contacto
-  // getById(id: number) {
-  //   return this.http.get<any>(`http://localhost:3000/api/contactos/${id}`);
-  // }
   getById(id: number) {
-  return this.http.get<any>(`${this.apiUrl}/${id}`);
-}
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
 }
